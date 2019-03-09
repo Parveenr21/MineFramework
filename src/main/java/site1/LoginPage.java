1d460e748dad698545ffee8dfe1fd0c85a6692da
parawat <parawat@egencia.com>
2019-03-09 17:18:03 +0530

    updated file

--- src/main/java/site1/LoginPage.java
+++ src/main/java/site1/LoginPage.java
@@ -7,6 +7,7 @@ import java.net.URL;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Action;
@@ -146,6 +147,9 @@ class
       	for(WebElement link:allLinks)
       	{
       		
+
+      		
+
       		System.out.println(link.getText());
       	}
       	
